d7571085679125f909ca97586b95fcee00820aa2
Naimatou-Batte <battekonate22@gmail.com>
2025-02-19 12:04:15 +0100

    Add files via upload

--- proje2.R
+++ proje2.R
@@ -1,5 +1,57 @@
 
+
+
+data <- read.csv("/home/UCA/nakonate/projet_croise/Tokyo_Wheather_Start.csv")
+
+data
+
+install.packages("dplyr")
 install.packages("tidyr")
+library(dplyr)
+library(tidyr)
+
+
+#pour voir si y'a des données manquante :
+
+is.na(data)
+
+#pour enlever les na : 
+donne <- na.omit(data)
+
+donne
+
+#pour voir la structure des données
+
+str(donne)
+# Convertir la colonne 'date' en type Date 
+
+donne$Date <- as.Date(donne$Date, format= "%d/%m/%Y")
+
+View(donne)
+# Si l'heure est présente dans une colonne séparée, vous pouvez aussi la convertir
+
+donne$Hour <- hms::as_hms(donne$Hour)
+
+str(donne)
+
+donne
+View(donne)
+# Si vous voulez regrouper par date et calculer la moyenne ou d'autres mesures
+data_group <- donne %>%
+  group_by(Date) %>%
+  summarise(
+    #heure_moyenne=mean(Hour),
+    temperature_moyenne = mean(Temperature), 
+    humidite_moyenne = mean(Humidity),  
+    vent_moyenne = mean(Wind.speed),  
+    nb_observations = n()  # Nombre d'observations par jour
+  )
+
+# Afficher le résultat
+print(data_group)
+
+
+
 
 data1 <- read.table("~/projet_croise/Tokyo_Scooter_Rental_Start(1).txt", quote="\"", comment.char="")
 data1
@@ -43,11 +95,14 @@ separated$Heure <- hms::as_hms(separated$Heure)
 separated_group <- separated %>%
   group_by(Date) %>%
   summarise(
-    heure_moyenne=mean(Heure),
+    #heure_moyenne=mean(Heure),
     nb_observations = n()  # Nombre d'observations par jour
   )
 
 print(separated_group)
 
 
+# Fusion des deux data par la colonne Date
 
+data_fusion <- merge(data_group, separated_group, by = "Date")
+data_fusion
